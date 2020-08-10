78719d823583578065d82730948e274451143f91
Yoshimasa Obayashi <obayashi.yoshimasa@seek-the-truth.org>
2020-08-10 11:21:47 +0900

    TENTATIVE: workaround of undefined reference to `tiled_to_planar'

---
+++ src/tiled_yuv.c
@@ -0,0 +1,12 @@
+void tiled_to_planar(void *src, void *dst, unsigned int dst_pitch,
+		     unsigned int width, unsigned int height)
+{
+	return;
+}
+
+void tiled_deinterleave_to_planar(void *src, void *dst1, void *dst2,
+				  unsigned int dst_pitch, unsigned int width,
+				  unsigned int height)
+{
+	return;
+}
